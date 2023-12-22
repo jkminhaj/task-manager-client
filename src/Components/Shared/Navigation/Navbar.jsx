438b1a0843c7d1bcf74afb5878d2bb6b5ae87767
Minhajul Islam <mmminhaj221@gmail.com>
2023-12-22 21:02:07 +0600

    fixed nav

--- src/Components/Shared/Navigation/Navbar.jsx
+++ src/Components/Shared/Navigation/Navbar.jsx
@@ -1,9 +1,10 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Login from "../../Auth/Login";
 import { useContext, useState } from "react";
 import Join from "../../Auth/Join";
 import { AuthContext } from "../../../Providers/AuthProvider";
 import avatar from '../../../assets/avatar.png'
+import task from '../../../assets/task.png'
 const Navbar = () => {
     const { showLogin, setShowLogin ,user ,logout} = useContext(AuthContext);
     const LoginHandler = () => {
@@ -28,7 +29,7 @@ const Navbar = () => {
                             {links}
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <Link to='/'><img className="w-12" src={task} alt="" /></Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
